e3cd919acb498f5598ab33680c0283bc5947b267
matt-pessoa <matheuspessoafonseca@gmail.com>
2021-12-27 22:40:33 -0300

    Stylize email btn

--- src/components/InfoSocial.js
+++ src/components/InfoSocial.js
@@ -1,10 +1,19 @@
 import React from "react";
+import emailIcon from "../images/email-icon.png";
 
 function InfoSocial() {
 	return (
 		<div className="social">
-			<button>Email</button>
-			<button>Linkedin</button>
+			<a href="mail.google.com" className="email-btn">
+				<img src={emailIcon} alt="email" />
+				Email
+			</a>
+			<a
+				href="https://www.linkedin.com/in/matt-pessoa/"
+				className="linkedin-btn"
+			>
+				Linkedin
+			</a>
 		</div>
 	);
 }
