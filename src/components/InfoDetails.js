12c2da6f975195030e4ca3bae1f0a3ea91123487
matt-pessoa <matheuspessoafonseca@gmail.com>
2021-12-27 23:08:04 -0300

    Stylize details

--- src/components/InfoDetails.js
+++ src/components/InfoDetails.js
@@ -4,9 +4,16 @@ function InfoDetails() {
 	return (
 		<div className="details">
 			<h3>About</h3>
-			<p>Lorem ipsum</p>
+			<p>
+				I am a frontend developer with a particular interest in making things
+				simple and always thinking about accessibility. I try to keep up with
+				the best practices, and I am always looking for new things to learn.
+			</p>
 			<h3>Interests</h3>
-			<p>Lorem ipsum</p>
+			<p>
+				Artist. Music. Reader. Internet fanatic. Travel geek. Pop culture ninja.
+				Tea explorer.
+			</p>
 		</div>
 	);
 }
